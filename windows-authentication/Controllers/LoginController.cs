cdaeb9220f1e2262fa8d3a55aeab5b913cc5ab8a
Lucas Viana Munhoz <lucas.munhoz@codeservicesconsulting.com>
2023-05-25 14:12:32 -0300

    Initial Commit
    first comit

---
+++ windows-authentication/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace windows_authentication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Usuário autenticado
+                return Ok();
+            }
+            else
+            {
+                // Usuário não autenticado
+                return Unauthorized();
+            }
+        }
+
+    }
+
+}
